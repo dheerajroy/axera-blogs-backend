94ef6e5065a1d004b86d58ca8cd765410da862b4
D Dheeraj <dhiraajroy123@gmail.com>
2024-02-20 18:06:31 +0530

    made few changes for making it open source

--- AxeraBlogs/settings.py
+++ AxeraBlogs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0qyooz6*-svm-c*@=6v850h7)5&xy$azz4+4ji*)mab+$_l24z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = False
@@ -85,26 +85,11 @@ WSGI_APPLICATION = 'AxeraBlogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'CONN_MAX_AGE': 3600,
-        'NAME': 'axeraBlogs',
-        'USER': 'axera',
-        'PASSWORD': 'dheeraj123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 # Password validation
--- blogsAggregator/views.py
+++ blogsAggregator/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from .serializers import BlogSerializer
 from .models import Blog
-
+import random
 
 class BlogViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Blog.objects.all().order_by('?')
+    queryset = Blog.objects.order_by('?')
     serializer_class = BlogSerializer
     filter_fields = ['topic__topic']
     search_fields = ['title', 'topic__topic']
